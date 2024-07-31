4d2bffb471edeb4ee0710f86a33421bd678e1184
Rapzzider <thecrisgb33@gmail.com>
2024-07-31 10:54:29 -0400

    Agregando el archivo de variable en pytho

---
+++ variables.py
@@ -0,0 +1,20 @@
+#Variable tipo entero
+a=10
+b=10
+print(a)
+print(b)
+#Variable flotantes
+c=1.5
+d=3.7
+suma = c + d
+print(suma)
+#variables tipo string
+nombre= "Roberto"
+apellido= "Hernandez"
+fullName = nombre + " " +apellido
+print(fullName)
+#variable tipo booleano
+lampara=True
+print(lampara)
+lampara=False
+print(lampara)
